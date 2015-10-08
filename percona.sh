7d88e0e8b6deb218ab104724fcd314c74fdec3c4
odenktools <odenktools86@gmail.com>
2015-10-08 23:14:52 +0700

    Fix repository bug

--- percona.sh
+++ percona.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-
-apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-
-echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list && \
+sudo apt-get update
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
 echo "deb-src http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
-
-apt-get update
-
-sudo apt-get install -y percona-xtradb-cluster-client-5.5 \
-		percona-xtradb-cluster-server-5.5 \
-		percona-xtrabackup percona-toolkit
-
+sudo apt-get update
+sudo apt-get install -y percona-xtradb-cluster-55  percona-xtradb-cluster-client-5.5 percona-xtradb-cluster-galera-2.x percona-xtrabackup percona-toolkit
 sudo mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 sudo mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 sudo mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
+
+## https://www.percona.com/downloads/Percona-XtraDB-Cluster/Percona-XtraDB-Cluster-5.5.41-25.11/binary/debian/trusty/x86_64/Percona-XtraDB-Cluster-5.5.41-25.11-r853-trusty-x86_64-bundle.tar
