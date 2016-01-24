31efaa3d9e20dc211cbfed0ed571a9829f016753
rahyan <rahyan.ramadhani@gmail.com>
2016-01-24 10:12:08 +0700

    Fix some code

--- percona.sh
+++ percona.sh
@@ -4,7 +4,7 @@ sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
 echo "deb-src http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
 sudo apt-get update
-sudo apt-get install -y percona-xtradb-cluster-55  percona-xtradb-cluster-client-5.5 percona-xtradb-cluster-galera-2.x percona-xtrabackup percona-toolkit
+sudo apt-get install -y percona-xtradb-cluster-55 percona-xtradb-cluster-client-5.5 percona-xtradb-cluster-galera-2.x percona-xtrabackup percona-toolkit
 sudo mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 sudo mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 sudo mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
